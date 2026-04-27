173e95f8be80d9c220370d8058e20e54c44cf8f8
Josephine Ding <25005876@siswa.um.edu.my>
2026-04-27 16:08:12 +0800

    feat: initialize core data models and manager classes for restaurant and user management

---
+++ user/RestaurantManager.java
@@ -0,0 +1,41 @@
+package user;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Restaurant;
+import java.util.HashMap;
+
+public class RestaurantManager {
+
+    private List<Restaurant> restaurants;
+    private HashMap<Integer, Restaurant> restaurantMap;
+
+    public RestaurantManager() {
+        this.restaurants = new ArrayList<>();
+        this.restaurantMap = new HashMap<>();
+    }
+
+    public void addRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+        restaurantMap.put(restaurant.getRestaurantID(), restaurant);
+        System.out.println("Restaurant added: " + restaurant.getName());
+    }
+
+    public void removeRestaurant(int restaurantID) {
+        restaurants.removeIf(restaurant -> restaurant.getRestaurantID() == restaurantID);
+        restaurantMap.remove(restaurantID);
+        System.out.println("Restaurant removed: " + restaurantID);
+    }
+
+    public void displayRestaurant() {
+        System.out.println("Restaurants:");
+        for (Restaurant restaurant : restaurants) {
+            System.out.println(restaurant);
+        }
+    }
+
+    public Restaurant searchRestaurant(int restaurantID) {
+        return restaurantMap.get(restaurantID);
+    }
+
+}
